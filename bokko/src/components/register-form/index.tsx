e514cd4190f7f30a240ebf0114706b32b76c5fd8
ivdr <ivan.derishev04@gmail.com>
2024-10-07 14:33:15 +0300

    hotfix

--- bokko/src/components/register-form/index.tsx
+++ bokko/src/components/register-form/index.tsx
@@ -50,7 +50,7 @@ const RegisterForm: React.FC = () => {
     };
 
     return (
-        <div className="flex flex-col items-center justify-center h-screen bg-gray-100 p-4">
+        <div className="flex flex-col items-center text-black justify-center h-screen bg-gray-100 p-4">
             <h1 className="text-2xl font-bold mb-4">Добро пожаловать в bokko bot</h1>
             <form onSubmit={handleRegister} className="bg-white shadow-md rounded-lg p-6 space-y-4 w-full max-w-md">
                 <div>
