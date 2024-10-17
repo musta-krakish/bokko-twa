9fc61354e5e7bb81ae2ee174afa29a63401c690e
ivdr <ivan.derishev04@gmail.com>
2024-10-17 19:14:14 +0300

    hotfix

--- bokko/src/lib/services/instance.ts
+++ bokko/src/lib/services/instance.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const getInstance = () => {
     return axios.create({
-        baseURL: "http:///hoenir.hopto.org/",
+        baseURL: "https://bokko.qwerty-almaty.kz/",
         timeout: 100000
     });
 }
