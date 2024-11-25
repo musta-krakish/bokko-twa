3fdda63311a1211994a1a33c11d38cbbec4d9c33
makkenzo <nekgo2009@gmail.com>
2024-11-25 21:49:17 +0500

    Improves UI components and workflow for creating and managing goals
    Updates page layouts and navigation for goals and AI assistance
    Refines user interface and introduces new components for better user experience
    Enhances workflow for creating and managing goals with improved accessibility and ease of use

--- bokko/src/app/ai/page.tsx
+++ bokko/src/app/ai/page.tsx
@@ -1,18 +1,21 @@
-"use client"
+'use client';
 
-import { ApiService } from "@/lib/services/api_service";
-import { useInitData } from "@telegram-apps/sdk-react";
-import { useRouter, useSearchParams } from "next/navigation";
-import React, { useEffect, useState } from "react";
-import ReactMarkdown from 'react-markdown';  // Поддержка Markdown
+import { Button } from '@/components/ui/button';
+import { ApiService } from '@/lib/services/api_service';
+import { useInitData } from '@telegram-apps/sdk-react';
+import { Loader2 } from 'lucide-react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+import { FaChevronLeft } from 'react-icons/fa';
+import ReactMarkdown from 'react-markdown'; // Поддержка Markdown
 
 function AiContent() {
     const initData = useInitData(true);
     const params = useSearchParams();
     const router = useRouter();
-    const [text, setText] = useState<string>("");
+    const [text, setText] = useState<string>('');
     const [loading, setLoading] = useState<boolean>(true);
-    const goal_id = params.get("goal_id");
+    const goal_id = params.get('goal_id');
 
     useEffect(() => {
         const fetchData = async () => {
@@ -37,7 +40,7 @@ function AiContent() {
                 const data = await ApiService.askGoal(goal_id, initDataStr);
                 setText(data.detail);
             } catch (error) {
-                console.error("Failed to fetch data", error);
+                console.error('Failed to fetch data', error);
             } finally {
                 setLoading(false);
             }
@@ -55,40 +58,49 @@ function AiContent() {
     };
 
     return (
-        <div className="container mx-auto text-black p-6">
-            {loading ? (
-                <div className="flex justify-center items-center h-screen">
-                    <h2 className="text-lg font-semibold">Загрузка...</h2>
+        <div className="max-h-max max-w-md mx-auto relative flex flex-col h-screen">
+            <div className="bg-secondary">
+                <div className="max-w-[97%] flex items-center py-4 mx-auto">
+                    <Button onClick={handleGoBack} className="text-lg font-semibold" size="icon" variant="ghost">
+                        <FaChevronLeft color="white" />
+                    </Button>
+                    <h2 className="text-lg text-white font-semibold">Помощь с ИИ</h2>
                 </div>
-            ) : (
-                <div className="max-w-2xl mx-auto">
-                    <h2 className="text-xl font-semibold mb-4">Результат Помощи ИИ</h2>
-                    <div className="bg-white p-4 shadow-md rounded-lg max-h-[75vh] overflow-auto">
-                        <ReactMarkdown className="whitespace-pre-wrap">{text}</ReactMarkdown>
+            </div>
+            <div className="p-4 w-full">
+                {loading ? (
+                    <div className="flex justify-center items-center h-screen">
+                        <Loader2 className="animate-spin" />
                     </div>
-                    <div className="mt-4 flex flex-col space-y-2">
-                        <button
-                            onClick={handleGoTask.bind(null, goal_id || "")}
-                            className="w-full p-2 bg-gray-400 text-white rounded-md transition"
-                        >
-                            Перейти к задачам
-                        </button>
-                        <button
-                            onClick={handleGoBack}
-                            className="w-full p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition"
-                        >
-                            Назад
-                        </button>
+                ) : (
+                    <div className="max-w-2xl mx-auto">
+                        <h2 className="text-xl font-semibold mb-4">Результат Помощи ИИ</h2>
+                        <div className="bg-white p-4 shadow-md rounded-lg max-h-[75vh] overflow-auto">
+                            <ReactMarkdown className="whitespace-pre-wrap">
+                                {text && text.length > 0 ? text : 'Нет результатов'}
+                            </ReactMarkdown>
+                        </div>
+                        <div className="mt-4 flex flex-col space-y-2">
+                            <Button
+                                onClick={handleGoTask.bind(null, goal_id || '')}
+                                className="w-full text-lg font-semibold"
+                            >
+                                Перейти к задачам
+                            </Button>
+                            <Button onClick={handleGoBack} variant="secondary" className="w-full text-lg font-semibold">
+                                Назад
+                            </Button>
+                        </div>
                     </div>
-                </div>
-            )}
+                )}
+            </div>
         </div>
     );
 }
 
 export default function Ai() {
     return (
-        <React.Suspense fallback={<div>Загрузка...</div>}>
+        <React.Suspense fallback={<Loader2 className="animate-spin" />}>
             <AiContent />
         </React.Suspense>
     );
--- bokko/src/app/goal/page.tsx
+++ bokko/src/app/goal/page.tsx
@@ -1,10 +1,15 @@
-"use client"
+'use client';
 
-import { ApiService } from "@/lib/services/api_service";
-import { Goal } from "@/lib/types";
-import { useInitData } from "@telegram-apps/sdk-react";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Textarea } from '@/components/ui/textarea';
+import { ApiService } from '@/lib/services/api_service';
+import { Goal } from '@/lib/types';
+import { useInitData } from '@telegram-apps/sdk-react';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { FaChevronLeft } from 'react-icons/fa';
 
 export default function Goals() {
     const initData = useInitData(true);
@@ -51,7 +56,7 @@ export default function Goals() {
             setGoalId(goal_response._id);
             setGoalCreated(true); // Устанавливаем флаг, что цель создана
         } catch (error) {
-            console.error("Failed to create goal:", error);
+            console.error('Failed to create goal:', error);
         }
     };
 
@@ -68,81 +73,75 @@ export default function Goals() {
     };
 
     return (
-        <div>
-            <div className="fixed inset-0 flex text-black items-center justify-center bg-opacity-50 z-50">
-                <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
-                    <div className="flex items-center space-x-2 mb-4">
-                        <button onClick={handleGoBack} className="text-lg font-semibold">
-                            &lt;
-                        </button>
-                        <h2 className="text-lg font-semibold">
-                            {goalCreated ? "Цель создана" : "Создать цель"}
-                        </h2>
-                    </div>
-                    {!goalCreated ? ( // Если цель еще не создана, показываем форму
-                        <form onSubmit={handleCreate} className="space-y-4">
-                            <div>
-                                <label className="block mb-1" htmlFor="title">Название:</label>
-                                <input
-                                    type="text"
-                                    id="title"
-                                    name="title"
-                                    value={goal.title}
-                                    onChange={handleChange}
-                                    required
-                                    className="w-full p-2 border border-gray-300 rounded-md"
-                                />
-                            </div>
-                            <div>
-                                <label className="block mb-1" htmlFor="description">Описание:</label>
-                                <textarea
-                                    id="description"
-                                    name="description"
-                                    value={goal.description}
-                                    onChange={handleChange}
-                                    required
-                                    className="w-full p-2 border border-gray-300 rounded-md"
-                                />
-                            </div>
-                            <div>
-                                <label className="block mb-1" htmlFor="deadline">Срок выполнения:</label>
-                                <input
-                                    type="date"
-                                    id="deadline"
-                                    name="deadline"
-                                    value={goal.deadline.toISOString().substring(0, 10)}
-                                    onChange={(e) => setGoal({ ...goal, deadline: new Date(e.target.value) })}
-                                    required
-                                    className="w-full p-2 border border-gray-300 rounded-md"
-                                />
-                            </div>
-                            <button
-                                type="submit"
-                                className="w-full p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition"
-                            >
-                                Создать Цель
-                            </button>
-                        </form>
-                    ) : ( // Если цель создана, показываем навигационные кнопки
-                        <div>
-                            <p className="text-center text-green-600 mb-4">Цель успешно создана!</p>
-                            <div className="mt-4 flex flex-col space-y-2">
-                                <button
-                                    onClick={handleAddTasks}
-                                    className="w-full p-2 bg-green-500 text-white rounded-md hover:bg-green-600 transition"
-                                >
-                                    Добавить задачи
-                                </button>
-                                <button
-                                    onClick={handleAiHelp}
-                                    className="w-full p-2 bg-purple-500 text-white rounded-md hover:bg-purple-600 transition"
-                                >
-                                    Помощь ИИ
-                                </button>
-                            </div>
-                        </div>
-                    )}
+        <div className="max-h-max max-w-md mx-auto relative flex flex-col h-screen">
+            <div className="bg-secondary">
+                <div className="max-w-[97%] flex items-center py-4 mx-auto">
+                    <Button onClick={handleGoBack} className="text-lg font-semibold" size="icon" variant="ghost">
+                        <FaChevronLeft color="white" />
+                    </Button>
+                    <h2 className="text-lg text-white font-semibold">Создать цель</h2>
                 </div>
+            </div>
+            <div className="p-4">
+                {!goalCreated ? ( // Если цель еще не создана, показываем форму
+                    <form onSubmit={handleCreate} className="space-y-4">
+                        <div>
+                            <Label className="block mb-1" htmlFor="title">
+                                Название:
+                            </Label>
+                            <Input
+                                type="text"
+                                id="title"
+                                name="title"
+                                value={goal.title}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        <div>
+                            <Label className="block mb-1" htmlFor="description">
+                                Описание:
+                            </Label>
+                            <Textarea
+                                id="description"
+                                name="description"
+                                value={goal.description}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        <div>
+                            <Label className="block mb-1" htmlFor="deadline">
+                                Срок выполнения:
+                            </Label>
+                            <Input
+                                type="date"
+                                id="deadline"
+                                name="deadline"
+                                value={goal.deadline.toISOString().substring(0, 10)}
+                                onChange={(e) => setGoal({ ...goal, deadline: new Date(e.target.value) })}
+                                required
+                                className="w-full p-2 border border-gray-300 rounded-md"
+                            />
+                        </div>
+                        <Button type="submit" className="w-full text-lg font-semibold">
+                            Создать Цель
+                        </Button>
+                    </form>
+                ) : (
+                    // Если цель создана, показываем навигационные кнопки
+                    <div>
+                        <p className="text-center text-green-600 mb-4">Цель успешно создана!</p>
+                        <div className="mt-4 flex flex-col space-y-2">
+                            <Button onClick={handleAddTasks} className="w-full text-lg font-semibold">
+                                Добавить задачи
+                            </Button>
+                            <Button variant="secondary" onClick={handleAiHelp} className="w-full text-lg font-semibold">
+                                Помощь ИИ
+                            </Button>
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     );
